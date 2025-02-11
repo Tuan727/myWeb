db121fe6dc7d6dc141820862dc5866f1d4d97cc7
Nguyễn Minh Tuấn <tuannguyengodry07@gmail.com>
2025-02-11 21:10:25 +0700

    1

--- api/register.php
+++ api/register.php
@@ -25,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $db->close();
 
     // Redirect to a success page (optional)
-    header("Location: login.html");
+    header("Location: ../ogin.html");
     exit();
 }
 ?>
